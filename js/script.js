4093752b374c96c99887bdcf13c242b255930014
EDNAN FRIZZERA FILHO <ednan@frizzera.dev>
2023-12-22 13:23:02 -0300

    Add update note functionality

--- js/script.js
+++ js/script.js
@@ -68,6 +68,11 @@ function createNote(id, content, fixed) {
 	}
 
 	// Note events (pin, delete and duplicate)
+	element.querySelector('textarea').addEventListener('keyup', (e) => {
+		const noteContent = e.target.value;
+		updateNote(id, noteContent);
+	});
+
 	element.querySelector('.bi-pin-angle').addEventListener('click', () => {
 		toggleFixNote(id);
 	});
@@ -121,6 +126,14 @@ function copyNote(id) {
 	notesContainer.appendChild(noteElement);
 
 	notes.push(noteObject);
+
+	saveNotes(notes);
+}
+
+function updateNote(id, newContent) {
+	const notes = getNotes();
+	const targetNote = notes.filter((note) => note.id === id)[0];
+	targetNote.content = newContent;
 
 	saveNotes(notes);
 }
